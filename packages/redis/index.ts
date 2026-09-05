1b15a035574b675db627d29378fa48c0c3caed83
Manassaha404 <manassaha425@gmail.com>
2026-09-05 22:14:16 +0530

    update Redis initialization to use environment variable for URL

--- packages/redis/index.ts
+++ packages/redis/index.ts
@@ -1,7 +1,9 @@
 import Redis from "ioredis";
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
-});
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: 6379,
+    });
 
 export default redis;
